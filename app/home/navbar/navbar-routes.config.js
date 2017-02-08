51a1ea138e1a701a4e3e9114d5d62a8ebcf86aea
Roar Fredriksen <roarfred@gmail.com>
2017-02-09 00:43:35 +0100

    Cleaned up more js and map

--- app/home/navbar/navbar-routes.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var navbar_metadata_1 = require("./navbar.metadata");
-exports.ROUTES = [
-    { path: '', title: 'XNSENSE', menuType: navbar_metadata_1.MenuType.BRAND, anonymous: true },
-    { path: 'components', title: 'Components', menuType: navbar_metadata_1.MenuType.LEFT, anonymous: false },
-    { path: 'login', title: 'Logout', menuType: navbar_metadata_1.MenuType.RIGHT, anonymous: false },
-    { path: 'login', title: 'Login', menuType: navbar_metadata_1.MenuType.RIGHT, anonymous: true }
-];
-//# sourceMappingURL=navbar-routes.config.js.map
