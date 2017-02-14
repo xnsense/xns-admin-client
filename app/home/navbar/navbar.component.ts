fe5643eec917208c2ef50d9da7c18acb5210d7ae
xnsense <JanCB@Jans-MacBook-Pro.local>
2017-02-14 21:45:07 +0100

    change login/logout

--- app/home/navbar/navbar.component.ts
+++ app/home/navbar/navbar.component.ts
@@ -26,6 +26,11 @@ export class NavbarComponent implements OnInit {
     this.isLoggedIn = this._service.isLoggedIn();
     this.menuItems = ROUTES.filter(menuItem => menuItem.menuType !== MenuType.BRAND && (menuItem.anonymous != this._service.isLoggedIn()));
     this.brandMenu = ROUTES.filter(menuItem => menuItem.menuType === MenuType.BRAND)[0];
+    this._service.onLogin.subscribe(v => {
+      if (v) {
+        this.isLoggedIn = true;
+      }
+    });
   }
 
   public get menuIcon(): string {
