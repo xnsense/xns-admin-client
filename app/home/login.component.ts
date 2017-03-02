f63c8fa940285aa88921f6a2d9254fdc57c576af
xnsense <JanCB@Jans-MacBook-Pro.local>
2017-03-03 00:22:36 +0100

    New login screen

--- app/home/login.component.ts
+++ app/home/login.component.ts
@@ -5,7 +5,8 @@ import { XnsService } from '../api/xns.service';
 
 @Component({
     moduleId: module.id,
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['login.component.css']
 })
 
 export class LoginComponent implements OnInit {
